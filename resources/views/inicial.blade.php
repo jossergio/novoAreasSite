7226787617e16246f1db4e9ebbab011ff4bc70f8
Jossérgio Soares Antas de Gouveia <jossergio@outlook.com.br>
2023-12-26 15:51:38 -0300

    Revertido um pequeno [des]ajuste no local da inserção do script calcular.js na base da página

--- resources/views/inicial.blade.php
+++ resources/views/inicial.blade.php
@@ -8,7 +8,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
-    <script src="calcular.js"></script>
     <link rel="stylesheet" href="calcular.css">
 </head>
 
@@ -59,4 +58,6 @@
 </div>
 </body>
 </html>
+
+<script src="calcular.js"></script>
 
